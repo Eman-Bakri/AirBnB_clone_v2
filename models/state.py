b6e233fd25bc3fabeba9cc8b5bbf2624b55d9809
Eman-Bakri <eman1996-sy@hotmail.com>
2024-01-27 23:27:06 +0000

    update2

--- models/state.py
+++ models/state.py
@@ -11,6 +11,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 
 
+
 class State(BaseModel, Base):
     """Class State"""
     __tablename__ = 'states'
@@ -18,21 +19,20 @@ class State(BaseModel, Base):
     cities = relationship('City', cascade='all, delete, delete-orphan',
                           backref='state')
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-    	@property
-    	def cities(self):
-            """cities getter"""
-            allv = models.storage.all()
-            _list_citi = []
-            _list_res = []
+    @property
+    def cities(self):
+        """cities getter"""
+        allv = models.storage.all()
+        _list_citi = []
+        _list_res = []
 
-            for kwd in allv:
-                city = kwd.replace('.', ' ')
-                city = shlex.split(city)
-                if (city[0] == 'City'):
-                    _list_citi.append(allv[kwd])
+        for kwd in allv:
+            city = kwd.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                _list_citi.append(allv[kwd])
 
-            for element in _list_citi:
-                if (element.state_id == self.id):
-                    _list_res.append(element)
-            return (_list_res)
+        for element in _list_citi:
+            if (element.state_id == self.id):
+                _list_res.append(element)
+        return (_list_res)
